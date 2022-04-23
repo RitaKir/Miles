2ba2b66b36ddda325138d116f94fa8d1c6aec0c7
RitaKir <kireeva.rita93@gmail.com>
2022-04-23 16:15:45 +0700

    added ,gitignore

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int amountInRubles = 10000;
+        int percentInRubles = 20;
+        int bonus = amountInRubles / percentInRubles;
+        System.out.println("Вам начисленно : " + bonus + " миль.");
+    }
+}
+
